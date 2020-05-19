a9425587041a7ef058ecd24e198ac85ff1411f80
PAWAN KUMAR <pawan@etho.io>
2020-05-19 17:26:57 +0530

    add framework

---
+++ Music/Music/EthoRequest.swift
@@ -0,0 +1,30 @@
+//
+//  EthoRequest.swift
+//  Music
+//
+//  Created by PAWAN KUMAR on 19/05/20.
+//  Copyright © 2020 Pawan co. All rights reserved.
+//
+
+import UIKit
+
+class EthoRequest: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Spotify Etho request")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Music/Music/Music.h
@@ -0,0 +1,19 @@
+//
+//  Music.h
+//  Music
+//
+//  Created by PAWAN KUMAR on 19/05/20.
+//  Copyright © 2020 Pawan co. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Music.
+FOUNDATION_EXPORT double MusicVersionNumber;
+
+//! Project version string for Music.
+FOUNDATION_EXPORT const unsigned char MusicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Music/PublicHeader.h>
+
+
